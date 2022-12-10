87e3ae236849ee5d04ab33c608dc0697114d25e0
minicloudsky <1397991131@qq.com>
2022-12-10 18:50:58 +0800

    feat: init react demo

---
+++ hello-react/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <h2>Hello React</h2>
+    </div>
+  );
+}
+
+export default App;
